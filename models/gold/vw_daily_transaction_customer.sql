ca1e23ace580bfb67d05827dbef99e349d869479
nguyenthedanh5699@gmail.com <nguyenthedanh5699@gmail.com>
2024-11-06 12:33:55 +0000

    Update

---
+++ models/gold/vw_daily_transaction_customer.sql
@@ -0,0 +1,9 @@
+{{ 
+    config(
+        materialized='view'
+    ) 
+}}
+
+select customer_id, year, month, day_time, protocol_fee_amount
+from {{ ref("daily_transaction_customer") }}
+order by customer_id, day_time
